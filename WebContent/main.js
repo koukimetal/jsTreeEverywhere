56919192d3d902657bee319440576cf0370b948a
comcometal <comcometal@gmail.com>
2016-05-06 21:41:49 +0900

    introduced a feature to change icon

--- WebContent/main.js
+++ WebContent/main.js
@@ -7,38 +7,60 @@ var setUp = function() {
 		"contextmenu": {
 			"items": function (o, cb) {
 				var defaultItems = $.jstree.defaults.contextmenu.items(o, cb);
-				
+
 				//If I can have undo and redo then auto save should be better
 				//Save
-				defaultItems.save = {
-					"separator_before"	: false,
-					"separator_after"	: false,
-					"label"				: "Save",
-					"action"			: function (data) {
-						var v = $('#my_dir').jstree(true).get_json('#', {flat:true, no_state:true});
-						var jsonForm = JSON.stringify(v);
-						localStorage.setItem('userDirectory', jsonForm);
-						alert("Saved");
-					}
-				};
-				
-				//Publish
-				defaultItems.publish = {
-					"separator_before"	: false,
-					"separator_after"	: false,
-					"label"				: "Publish",
-					"action"			: function (data) {
-						var inst = $.jstree.reference(data.reference),
-						obj = inst.get_node(data.reference);
-						var v = $('#my_dir').jstree(true).get_json(obj.id);
-						v = jsTreeJsonMaker(v);
-						var jsonForm = JSON.stringify(v);
-						var snippet = snippetMaker(jsonForm);
+				defaultItems.save = makeNormalItem("Save", function (data) {
+					var v = $('#my_dir').jstree(true).get_json('#', {flat:true, no_state:true});
+					var jsonForm = JSON.stringify(v);
+					localStorage.setItem('userDirectory', jsonForm);
+					alert("Saved");
+				});
 
-						$("#snippet_result").val(snippet);
-					}
+				//Publish
+				defaultItems.publish = makeNormalItem("Publish", function (data) {
+					var inst = $.jstree.reference(data.reference),
+					obj = inst.get_node(data.reference);
+					var v = $('#my_dir').jstree(true).get_json(obj.id);
+					v = jsTreeJsonMaker(v);
+					var jsonForm = JSON.stringify(v);
+					var snippet = snippetMaker(jsonForm);
+
+					$("#snippet_result").val(snippet);
+				});
+
+				//Create item with icon
+				defaultItems.create.submenu = {
+						"directory" : makeNormalItem("Directory", function (data) {
+							var inst = $.jstree.reference(data.reference),
+							obj = inst.get_node(data.reference);
+							inst.create_node(obj, {"icon" : "glyphicon glyphicon-folder-open"}, "last", function (new_node) {
+								setTimeout(function () { inst.edit(new_node); },0);
+							});
+						}),
+						"file" : makeNormalItem("File", function (data) {
+							var inst = $.jstree.reference(data.reference),
+							obj = inst.get_node(data.reference);
+							inst.create_node(obj, {"icon" : "glyphicon glyphicon-file"}, "last", function (new_node) {
+								setTimeout(function () { inst.edit(new_node); },0);
+							});
+						}),
+						"image" : makeNormalItem("Image", function (data) {
+							var inst = $.jstree.reference(data.reference),
+							obj = inst.get_node(data.reference);
+							inst.create_node(obj, {"icon" : "glyphicon glyphicon-picture"}, "last", function (new_node) {
+								setTimeout(function () { inst.edit(new_node); },0);
+							});
+						}),
+						"log" : makeNormalItem("Log", function (data) {
+							var inst = $.jstree.reference(data.reference),
+							obj = inst.get_node(data.reference);
+							inst.create_node(obj, {"icon" : "glyphicon glyphicon-floppy-disk"}, "last", function (new_node) {
+								setTimeout(function () { inst.edit(new_node); },0);
+							});
+						})
 				};
-				
+
 				//To prevent some action to a root
 				if (o.id == "root") {
 					delete defaultItems.rename;
@@ -47,12 +69,47 @@ var setUp = function() {
 					delete defaultItems.ccp.submenu.copy;
 					delete defaultItems.publish;
 				}
-				
+
 				return defaultItems;
 			}
 		},
-		"plugins" : [ "contextmenu", "state", "dnd"]
+		"plugins" : [ "contextmenu", "state", "dnd", "types"]
 	});
+}
+
+//TODO implement what I want! In progress
+//I want to minify where I make icon items.
+//var makeIconSubmenu = function() {
+//	list = [
+//		["directory", "Directory", "glyphicon glyphicon-folder-open"],
+//		["directory", "Directory", "glyphicon glyphicon-folder-open"]
+//	];
+//	console.log(list[1][2]);
+//	var obj = {};
+//	console.log(list.length);
+//	for(var i = 0; i < list.length; i++) {
+//		console.log(i);
+//		console.log(list[i][1]);
+//		obj[list[i][0]] = makeNormalItem(list[i][1], function (data) {
+//			var inst = $.jstree.reference(data.reference),
+//			obj = inst.get_node(data.reference);
+//			console.log(list[i]);
+//			inst.create_node(obj, {"icon" : list[i][2]}, "last", function (new_node) {
+//				setTimeout(function () { inst.edit(new_node); },0);
+//			});
+//		})
+//	}
+//	return obj;
+//}
+
+var makeNormalItem = function(label, action) {
+	var obj = {
+			"separator_before"	: false,
+			"separator_after"	: false,
+			"label"				: label,
+			"action"			: action
+	};
+	return obj;
 }
 
 var jsTreeJsonMaker = function(data) {
@@ -73,9 +130,9 @@ var snippetMaker = function(jsonForm) {
 		jsonForm +
 		")";
 	var treeScript = 
-			"<script>\n" +
-			jsTreeCommand + ";\n" +
-			"</script>";
+		"<script>\n" +
+		jsTreeCommand + ";\n" +
+		"</script>";
 	return treeDiv + treeScript;
 }
 
