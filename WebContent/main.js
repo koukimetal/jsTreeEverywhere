5ad656a242bdd96842a6d5e972904c1f48704767
comcometal <comcometal@gmail.com>
2016-05-09 21:17:17 +0900

    Refactored and introduced change icon and removed open is false from json output

--- WebContent/main.js
+++ WebContent/main.js
@@ -1,3 +1,5 @@
+//INIT
+
 var setUp = function() {
 	$("#my_dir").jstree({
 		"core" : {
@@ -8,61 +10,24 @@ var setUp = function() {
 			"items": function (o, cb) {
 				var defaultItems = $.jstree.defaults.contextmenu.items(o, cb);
 
+				//Change icon
+				defaultItems.changeIcon = makeNormalItem("ChangeIcon", false);
+				defaultItems.changeIcon.submenu = makeIconChangeSubmenu(iconsData);
+				
 				//If I can have undo and redo then auto save should be better
 				//Save
 				defaultItems.save = makeNormalItem("Save", function (data) {
-					var v = $('#my_dir').jstree(true).get_json('#', {flat:true, no_state:true});
-					var jsonForm = JSON.stringify(v);
-					localStorage.setItem('userDirectory', jsonForm);
-					alert("Saved");
+					saveJsTreeData();
 				});
 
 				//Publish
 				defaultItems.publish = makeNormalItem("Publish", function (data) {
-					var inst = $.jstree.reference(data.reference),
-					obj = inst.get_node(data.reference);
-					var rawJson = $('#my_dir').jstree(true).get_json(obj.id);
-					
-					var treeId = $('#treeId').val();
-					if(treeId == "") {
-						treeId = 'tree_' + Math.floor(Math.random() * 1000);
-					}
-					
-					var trimmedJsTreeData = makeTrimmedObject(rawJson, 0, treeId)[1];
-					var jsTree = makeJsTree(treeId, trimmedJsTreeData);
-					
-					var jsTreeJson = JSON.stringify(jsTree);
-					var jsonSnippet = snippetMaker(treeId, "", jsTreeJson);
-
-					$("#snippetJSON").val(jsonSnippet);
-					
-					var jsTreeHTML = makeJsTreeHTML(treeId, trimmedJsTreeData);
-					var htmlSnippet = snippetMaker(treeId, jsTreeHTML, "");
-					$("#snippetHTML").val(htmlSnippet);
+					publishJsTree(data);
 				});
 
 				//Create item with icon
-				defaultItems.create.submenu = makeIconSubmenu(
-						{
-							"directory" : {
-								"label" : "Directory",
-								"icon" : "icon-folder-alt"
-							},
-							"file" : {
-								"label" : "File",
-								"icon" : "icon-doc"
-							},
-							"image" : {
-								"label" : "Image",
-								"icon" : "icon-picture"
-							},
-							"disc" : {
-								"label" : "Disc",
-								"icon" : "icon-disc"
-							}
-						}	
-				);
-
+				defaultItems.create.submenu = makeIconSubmenu(iconsData);
+				
 				//To prevent some action to a root
 				if (o.id == "root") {
 					delete defaultItems.rename;
@@ -70,6 +35,7 @@ var setUp = function() {
 					delete defaultItems.ccp.submenu.cut;
 					delete defaultItems.ccp.submenu.copy;
 					delete defaultItems.publish;
+					delete defaultItems.changeIcon;
 				}
 
 				return defaultItems;
@@ -79,42 +45,20 @@ var setUp = function() {
 	});
 }
 
+var loadUserDir = function() {
+	var jsonString = localStorage.getItem('userDirectory');
+	var jsonData = JSON.parse(jsonString);
+	if (jsonData == null) {
+		jsonData = [{id: "root", text:"root"}];
+	}
+	return jsonData;
+}
+
 var setUpHandlebars = function() {
 	Handlebars.registerPartial( "tree-template", $( "#tree-template" ).html() );
 }
 
-var makeJsTreeHTML = function(treeId, trimmedData) {
-	var main = Handlebars.compile( $( "#main-template" ).html());
-	var html = main( {"children": [trimmedData]} );
-//	return html;
-	return html.replace(/>\s+</g, "><").replace(/\s+</g, "<").replace(/>\s+/g, ">");
-}
-
-var makeIconSubmenu = function(items) {
-	var resobj = {};
-	$.each(items, function(key, value) {
-		var itemFuncion = function (data) {
-			var inst = $.jstree.reference(data.reference),
-			obj = inst.get_node(data.reference);
-			inst.create_node(obj, {"icon" : value.icon}, "last", function (new_node) {
-				setTimeout(function () { inst.edit(new_node); },0);
-			});
-		};
-		resobj[key] = makeNormalItem(value.label, itemFuncion);
-	});
-
-	return resobj;
-}
-
-var makeNormalItem = function(label, action) {
-	var obj = {
-			"separator_before"	: false,
-			"separator_after"	: false,
-			"label"				: label,
-			"action"			: action
-	};
-	return obj;
-}
+//OUTPUT
 
 var makeTrimmedObject = function(root, nodeId, prefix) {
 	var obj = {};
@@ -123,8 +67,12 @@ var makeTrimmedObject = function(root, nodeId, prefix) {
 	if(root.icon !== true) {
 		obj.icon = root.icon;
 	}
-	obj.state = {};
-	obj.state.opened = root.state.opened;
+	
+	if(root.state.opened === true) {
+		obj.state = {};
+		obj.state.opened = true;
+	}
+	
 	var size = 1;
 	if(root.children.length > 0) {
 		obj.children = [];
@@ -134,7 +82,7 @@ var makeTrimmedObject = function(root, nodeId, prefix) {
 			obj.children[i] = v[1];
 		}
 	}
-	
+
 	return [size, obj];
 }
 
@@ -160,11 +108,104 @@ var snippetMaker = function(treeId, htmlForm, jsonForm) {
 	return treeDiv + treeScript;
 }
 
-var loadUserDir = function() {
-	var jsonString = localStorage.getItem('userDirectory');
-	var jsonData = JSON.parse(jsonString);
-	if (jsonData == null) {
-		jsonData = [{id: "root", text:"root"}];
+var makeJsTreeHTML = function(treeId, trimmedData) {
+	var main = Handlebars.compile( $( "#main-template" ).html());
+	var html = main( {"children": [trimmedData]} );
+//	return html;
+	return html.replace(/>\s+</g, "><").replace(/\s+</g, "<").replace(/>\s+/g, ">");
+}
+
+//MENU
+
+var saveJsTreeData = function() {
+	var v = $('#my_dir').jstree(true).get_json('#', {flat:true, no_state:true});
+	var jsonForm = JSON.stringify(v);
+	localStorage.setItem('userDirectory', jsonForm);
+	alert("Saved");
+}
+
+var publishJsTree = function(data) {
+	var inst = $.jstree.reference(data.reference),
+	obj = inst.get_node(data.reference);
+	var rawJson = $('#my_dir').jstree(true).get_json(obj.id);
+
+	var treeId = $('#treeId').val();
+	if(treeId == "") {
+		treeId = 'tree_' + Math.floor(Math.random() * 1000);
 	}
-	return jsonData;
+
+	var trimmedJsTreeData = makeTrimmedObject(rawJson, 0, treeId)[1];
+	var jsTree = makeJsTree(treeId, trimmedJsTreeData);
+
+	var jsTreeJson = JSON.stringify(jsTree);
+	var jsonSnippet = snippetMaker(treeId, "", jsTreeJson);
+
+	$("#snippetJSON").val(jsonSnippet);
+
+	var jsTreeHTML = makeJsTreeHTML(treeId, trimmedJsTreeData);
+	var htmlSnippet = snippetMaker(treeId, jsTreeHTML, "");
+	$("#snippetHTML").val(htmlSnippet);
+}
+
+var iconsData = {
+		"directory" : {
+			"label" : "Directory",
+			"icon" : "icon-folder-alt"
+		},
+		"file" : {
+			"label" : "File",
+			"icon" : "icon-doc"
+		},
+		"image" : {
+			"label" : "Image",
+			"icon" : "icon-picture"
+		},
+		"disc" : {
+			"label" : "Disc",
+			"icon" : "icon-disc"
+		},
+		"normal" : {
+			"label" : "Normal",
+			"icon" : true
+		}
+}
+
+var makeIconSubmenu = function(items) {
+	var resobj = {};
+	$.each(items, function(key, value) {
+		var itemFuncion = function (data) {
+			var inst = $.jstree.reference(data.reference),
+			obj = inst.get_node(data.reference);
+			inst.create_node(obj, {"icon" : value.icon}, "last", function (new_node) {
+				setTimeout(function () { inst.edit(new_node); },0);
+			});
+		};
+		resobj[key] = makeNormalItem(value.label, itemFuncion);
+	});
+
+	return resobj;
+}
+
+var makeIconChangeSubmenu = function(items) {
+	var resobj = {};
+	$.each(items, function(key, value) {
+		var itemFuncion = function (data) {
+			var inst = $.jstree.reference(data.reference),
+			obj = inst.get_node(data.reference);
+			inst.set_icon(obj, value.icon);
+		};
+		resobj[key] = makeNormalItem(value.label, itemFuncion);
+	});
+	
+	return resobj;
+}
+
+var makeNormalItem = function(label, action) {
+	var obj = {
+			"separator_before"	: false,
+			"separator_after"	: false,
+			"label"				: label,
+			"action"			: action
+	};
+	return obj;
 }
